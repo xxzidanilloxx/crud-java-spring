1e053b6ce59caa5e3dde1053a3c508ac77afd5b0
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-02-14 05:30:25 -0300

    feat: implementação de anotações que impõem restrições nos campos nome, cpf e email

--- src/main/java/com/danillo/crud/model/Usuario.java
+++ src/main/java/com/danillo/crud/model/Usuario.java
@@ -18,7 +18,13 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(unique = true, nullable = false)
     private  Long cpf;
+
+    @Column(unique = true, nullable = false)
     private String email;
 }
